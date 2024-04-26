198e5a9f43f7024c7d040d52df042f570396502d
Nagaraju1431 <mudenagarajunaik22@gmail.com>
2024-04-27 02:12:14 +0530

    files

---
+++ src/test/java/NewFiles.java
@@ -0,0 +1,16 @@
+import org.testng.annotations.Test;
+
+public class NewFiles {
+
+	@Test
+	public void newTextFiel() {
+
+		System.out.println("New Text File");
+	}
+
+	@Test
+	public void newVideoFile() {
+
+		System.out.println("New Video File");
+	}
+}
