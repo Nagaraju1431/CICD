f81728fc69dbef0f02bd4ff54913d7f98d0152bc
Nagaraju1431 <mudenagarajunaik22@gmail.com>
2024-04-27 00:54:17 +0530

    for CICD pipeline

---
+++ src/test/java/CriticalTest.java
@@ -0,0 +1,35 @@
+import org.testng.annotations.Test;
+
+public class CriticalTest {
+
+	@Test
+	public void Regression1() {
+
+		System.out.println("Regression1");
+	}
+
+	@Test
+	public void Regression2() {
+
+		System.out.println("Regression1");
+	}
+
+	@Test
+	public void Regression3() {
+
+		System.out.println("Regression1");
+	}
+
+	@Test
+	public void Regression4() {
+
+		System.out.println("Regression1");
+	}
+
+	@Test
+	public void Regression5() {
+
+		System.out.println("Regression1");
+	}
+
+}
